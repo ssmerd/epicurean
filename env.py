daf2788ff3540c6a8bf4dd4812092b0858ee2b0b
Sebastian Smerd <sebastiansmerd@Services-MBP.station>
2024-01-20 20:52:30 +0000

    Deployment Commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://danyarol:MWTQlBNrNG-l2QiyIpLn1RFbWXfW9abC@manny.db.elephantsql.com/danyarol"
+os.environ["SECRET_KEY"] = "django-insecure-)irk9amh*cqkro+p^p^0nb=+a2dh3ogsolib-04*t%saamzg5a"
+os.environ["CLOUDINARY_URL"] = "cloudinary://452715239737996:wm-4M0nbs6J48Au8HvDT0jxuSU4@dntxbpouu"
